3c322c40d3936722207db0e11b0f52691c1bfdd7
Taran van Groenigen <tvangroenigen@outlook.com>
2020-11-13 12:03:21 +0100

    Add Interfaces

---
+++ ContainerShip/Interfaces/IContainerRow.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContainerShip.Interfaces
+{
+	interface IContainerRow
+	{
+		IContainerColomn[] Colomns { get; }
+		int TotalWeight { get; }
+	}
+}
---
+++ ContainerShip/Interfaces/IContainerShip.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContainerShip.Interfaces
+{
+	interface IContainerShip
+	{
+		int Width { get; }
+		int Length { get; }
+		IContainerRow[] ContainerRows { get; }
+	}
+}
