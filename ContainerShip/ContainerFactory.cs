47e94450aa431e6e70214d2daecacc1abe19e293
Taran van Groenigen <tvangroenigen@outlook.com>
2020-11-13 12:18:11 +0100

    Add ContainerFactory

---
+++ ContainerShip/ContainerFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ContainerShip.Interfaces;
+using ContainerShip.Enums;
+
+namespace ContainerShip
+{
+	public static class ContainerFactory
+	{
+		static public IContainer Create(FreightType freightType, int weight)
+		{
+			switch (freightType)
+			{
+				case FreightType.Normal:
+					return new Container(weight);
+				case FreightType.Refrigerated:
+					return new RefrigeratedContainer(weight);
+				case FreightType.Valuable:
+					return new ValuableContainer(weight);
+				default:
+					throw new NotImplementedException();
+			}
+		}
+	}
+}
---
+++ Tests/ContainerFactoryTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ContainerShip;
+using ContainerShip.Interfaces;
+using ContainerShip.Enums;
+
+namespace Tests
+{
+	[TestClass]
+	public class ContainerFactoryTest
+	{
+		[TestMethod]
+		public void CreateNormalContainer()
+		{
+			IContainer container = ContainerFactory.Create(FreightType.Normal, 0);
+			Assert.IsInstanceOfType(container, typeof(Container));
+		}
+
+		[TestMethod]
+		public void CreateRefrigeratedContainer()
+		{
+			IContainer container = ContainerFactory.Create(FreightType.Refrigerated, 0);
+			Assert.IsInstanceOfType(container, typeof(RefrigeratedContainer));
+		}
+
+		[TestMethod]
+		public void CreateValuableContainer()
+		{
+			IContainer container = ContainerFactory.Create(FreightType.Valuable, 0);
+			Assert.IsInstanceOfType(container, typeof(ValuableContainer));
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(NotImplementedException))]
+		public void CreateInvalidContainer()
+		{
+			IContainer container = ContainerFactory.Create((FreightType)1000, 0);
+		}
+	}
+}
